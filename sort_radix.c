25a0dc1441351faa92b5f81af01414894d424112
daniel <daniel-congo2@hotmail.com>
2024-12-04 15:03:25 +0100

    sort3.c fini, sort5.c en cours fonction trier par bits

--- sort_radix.c
+++ sort_radix.c
@@ -6,10 +6,38 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 19:34:40 by daniel149af       #+#    #+#             */
-/*   Updated: 2024/12/02 19:35:40 by daniel149af      ###   ########.fr       */
+/*   Updated: 2024/12/03 20:37:44 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	find_max_bits(int max)
+{
+	int	max_bits;
 
+	max_bits = 0;
+	while (max > 0)
+	{
+		max >>= 1;
+		max_bits++;
+	}
+	return (max_bits);
+}
+
+void	sort_radix(t_stack **stack_a)
+{
+	int	max;
+	int	max_bits;
+	int	i;
+	int	j;
+
+	max = find_max(*stack_a);
+	max_bits = find_max_bits(max);
+	i = 0;
+	while (i < max_bits)
+	{
+		/* code */
+	}
+
+}
